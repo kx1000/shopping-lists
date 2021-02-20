6cd259689716d3bac7131566d51c4a9889eb5670
Kacper <kacper.rogula@gmail.com>
2021-02-20 19:47:30 +0100

    Install bootstrap

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,7 @@
 import 'jquery/dist/jquery.min';
 import 'symfony-collection/jquery.collection';
-import './styles/app.css';
+import 'bootstrap';
+import './styles/app.scss';
 import './bootstrap';
 import init from "./init";
 init.init();
